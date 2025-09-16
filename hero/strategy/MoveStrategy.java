8298009d4afeee5221f02aa63a623199778cab20
bozhkoDan21 <-email@--github.com>
2025-09-16 23:38:53 +0300

    update struct project lab1(2)

---
+++ hero/strategy/MoveStrategy.java
@@ -0,0 +1,10 @@
+package hero.strategy;
+
+/**
+ * Интерфейс стратегии перемещения героя
+ * Паттерн "Стратегия"
+ */
+public interface MoveStrategy {
+    int move();
+    String getDescription();
+}
